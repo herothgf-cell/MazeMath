052cb8f9b99499ac68f56b93c555fbd079c29684
herothgf-cell <herothgf@gmail.com>
2026-09-22 23:53:22 +0900

    feat: add knowledge XP and enchant services

---
+++ Assets/MazeMath/Scripts/Enchant/RobotEnchantIds.cs
@@ -0,0 +1,16 @@
+namespace MazeMath.Enchant
+{
+    public static class RobotEnchantIds
+    {
+        public const string MiningEcho = "enchant.mining.echo";
+        public const string MiningLucky = "enchant.mining.lucky";
+        public const string WrenchQuickFix = "enchant.wrench.quick-fix";
+        public const string WrenchCircuitSense = "enchant.wrench.circuit-sense";
+        public const string JumpSoftLanding = "enchant.jump.soft-landing";
+        public const string JumpRouteScan = "enchant.jump.route-scan";
+        public const string ShieldRecharge = "enchant.shield.recharge";
+        public const string ShieldStableField = "enchant.shield.stable-field";
+        public const string SensorMemory = "enchant.sensor.memory";
+        public const string SensorGuide = "enchant.sensor.guide";
+    }
+}
