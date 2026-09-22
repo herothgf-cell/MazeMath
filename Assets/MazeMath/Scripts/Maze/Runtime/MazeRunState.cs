1505fbeb7f0e36f2946f88013e6185d47b700c72
herothgf-cell <herothgf@gmail.com>
2026-09-22 19:27:10 +0900

    feat: add maze runtime service

---
+++ Assets/MazeMath/Scripts/Maze/Runtime/MazeRunState.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace MazeMath.Maze.Runtime
+{
+    [Serializable]
+    public sealed class MazeRunState
+    {
+        public string chapterId;
+        public int runSeed;
+        public string currentNodeId;
+        public int currentFloor;
+        public List<string> visitedNodeIds = new List<string>();
+        public List<string> seenNodeIds = new List<string>();
+        public List<string> solvedGateIds = new List<string>();
+        public string activeObjectiveId;
+        public string playerSpawnId;
+    }
+}
