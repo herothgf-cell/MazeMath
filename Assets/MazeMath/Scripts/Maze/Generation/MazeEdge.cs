870104ffa0c39c75d44851a06e5ff3d6a5a04911
herothgf-cell <herothgf@gmail.com>
2026-09-22 17:15:44 +0900

    feat: add maze edge model

---
+++ Assets/MazeMath/Scripts/Maze/Generation/MazeEdge.cs
@@ -0,0 +1,38 @@
+using System;
+using MazeMath.Maze.Data;
+
+namespace MazeMath.Maze.Generation
+{
+    public sealed class MazeEdge
+    {
+        public MazeEdge(
+            string edgeId,
+            string fromNodeId,
+            string toNodeId,
+            EdgeType type,
+            string? gateId,
+            bool isBidirectional)
+        {
+            if (string.IsNullOrWhiteSpace(edgeId))
+                throw new ArgumentException("Edge ID is required.", nameof(edgeId));
+            if (string.IsNullOrWhiteSpace(fromNodeId))
+                throw new ArgumentException("From node ID is required.", nameof(fromNodeId));
+            if (string.IsNullOrWhiteSpace(toNodeId))
+                throw new ArgumentException("To node ID is required.", nameof(toNodeId));
+
+            EdgeId = edgeId;
+            FromNodeId = fromNodeId;
+            ToNodeId = toNodeId;
+            Type = type;
+            GateId = gateId;
+            IsBidirectional = isBidirectional;
+        }
+
+        public string EdgeId { get; }
+        public string FromNodeId { get; }
+        public string ToNodeId { get; }
+        public EdgeType Type { get; }
+        public string? GateId { get; }
+        public bool IsBidirectional { get; }
+    }
+}
