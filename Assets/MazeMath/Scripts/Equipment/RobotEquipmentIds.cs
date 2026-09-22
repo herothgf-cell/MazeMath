0491b26f41071cd5975f422278c2085252e171a9
herothgf-cell <herothgf@gmail.com>
2026-09-22 23:50:53 +0900

    feat: add robot equipment service

---
+++ Assets/MazeMath/Scripts/Equipment/RobotEquipmentIds.cs
@@ -0,0 +1,17 @@
+namespace MazeMath.Equipment
+{
+    public static class RobotEquipmentIds
+    {
+        public const string MiningArm = "equipment.mining-arm.t1";
+        public const string PowerWrench = "equipment.power-wrench.t1";
+        public const string JumpBooster = "equipment.jump-booster.t1";
+        public const string EnergyShield = "equipment.energy-shield.t1";
+        public const string ExplorerSensor = "equipment.explorer-sensor.t1";
+
+        public const string BreakCrackedWall = "ability.break.cracked_wall";
+        public const string RepairBasicMachine = "ability.repair.basic_machine";
+        public const string JumpHighPlatform = "ability.jump.high_platform";
+        public const string ShieldHazard = "ability.shield.hazard";
+        public const string SensorInteraction = "ability.sensor.interaction";
+    }
+}
