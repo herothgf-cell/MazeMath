9c74a227e5df3e54df86e18ccb541caed22518dc
herothgf-cell <herothgf@gmail.com>
2026-09-22 23:56:29 +0900

    feat: add Chapter 1 content catalog

---
+++ Assets/MazeMath/Scripts/Content/ContentIds.cs
@@ -0,0 +1,25 @@
+namespace MazeMath.Content
+{
+    public static class ContentIds
+    {
+        public const string Scrap = "material.scrap";
+        public const string IronPlate = "material.iron-plate";
+        public const string Gear = "material.gear";
+        public const string EnergyCrystal = "material.energy-crystal";
+        public const string RareCore = "material.rare-core";
+
+        public const string MiningArmItem = "item.mining-arm";
+        public const string PowerWrenchItem = "item.power-wrench";
+        public const string JumpBoosterItem = "item.jump-booster";
+        public const string EnergyShieldItem = "item.energy-shield";
+        public const string ExplorerSensorItem = "item.explorer-sensor";
+
+        public const string MiningRecipe = "recipe.mining-arm";
+        public const string WrenchRecipe = "recipe.power-wrench";
+        public const string JumpRecipe = "recipe.jump-booster";
+        public const string ShieldRecipe = "recipe.energy-shield";
+        public const string SensorRecipe = "recipe.explorer-sensor";
+        public const string SensorPatternRecipe = "recipe.explorer-sensor.pattern";
+        public const string MiningPatternRecipe = "recipe.mining-arm.pattern";
+    }
+}
