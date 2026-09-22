2d4fe32c7c986c1313f17a4e405ffbe7b743db17
herothgf-cell <herothgf@gmail.com>
2026-09-23 00:33:38 +0900

    test: avoid assuming built-in font display name

--- Assets/MazeMath/Tests/EditMode/UI/RuntimeFontProviderTests.cs
+++ Assets/MazeMath/Tests/EditMode/UI/RuntimeFontProviderTests.cs
@@ -6,12 +6,11 @@ namespace MazeMath.Tests.UI
     public sealed class RuntimeFontProviderTests
     {
         [Test]
-        public void Get_ReturnsBuiltInLegacyRuntimeFont()
+        public void Get_ReturnsBuiltInRuntimeFont()
         {
             var font = RuntimeFontProvider.Get();
 
             Assert.IsNotNull(font);
-            Assert.AreEqual("LegacyRuntime", font.name);
         }
     }
 }
