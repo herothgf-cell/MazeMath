e1288b550bc7bab12789166feaf3823a810eb038
herothgf-cell <herothgf@gmail.com>
2026-09-22 17:15:38 +0900

    feat: add maze edge types

---
+++ Assets/MazeMath/Scripts/Maze/Data/EdgeType.cs
@@ -0,0 +1,14 @@
+namespace MazeMath.Maze.Data
+{
+    public enum EdgeType
+    {
+        Open,
+        Door,
+        Ladder,
+        Elevator,
+        Locked,
+        PuzzleLocked,
+        EquipmentLocked,
+        Hidden
+    }
+}
